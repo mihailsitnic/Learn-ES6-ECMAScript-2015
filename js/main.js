7c6a921ac725f86ebbd5393aedf84f2c72d8f8d0
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-01 20:29:14 +0200

    01-egghead-arrow-function__second

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var createGreeting = function(message, name) {
-  return message + name;
+var deliveryBoy = {
+  name: 'John',
+  handleMessage: function(message, handler) {
+    handler(message);
+  },
+  receive: function() {
+    this.handleMessage('Hello ', message => console.log(message + this.name))
+  }
 }
 
-var arrowGreeting = (message, name) => message + name;
+deliveryBoy.receive();
