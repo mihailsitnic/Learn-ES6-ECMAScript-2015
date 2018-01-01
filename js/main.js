8d3cd2741c02a226e23af1567a02b3a53a905702
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-01 20:51:44 +0200

    03-egghead-ecmascript-6-default-values-for-function-parameters

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 'use strict';
 
-function varFunc() {
-  var previos = 0;
-  var current = 1;
-  var i;
-  var temp;
-  for (i = 0; i < n; i += 1) {
-    temp = previos;
-    previos = current;
-    current = temp + current;
-  }
-}
-
-function letFunc() {
-  let previos = 0;
-  let current = 1;
-  for (let i = 0; i < n; i += 1) {
-    let temp = previos;
-    previos = current;
-    current = temp + current;
-  }
-}
+let recive = (complete = () => console.log('complete')) => complete();
+recive();
