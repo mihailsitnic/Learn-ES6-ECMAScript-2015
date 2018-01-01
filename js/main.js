39e18e2ecc0fd35b981f937c94c8eb3da42b8e31
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-01 20:44:38 +0200

    02-egghead-the-let-keyword__second

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 'use strict';
 
-var fs = [];
-
-for (let i = 0; i < 10; i++) {
-  fs.push(function() {
-    console.log(i);
-  })
+function varFunc() {
+  var previos = 0;
+  var current = 1;
+  var i;
+  var temp;
+  for (i = 0; i < n; i += 1) {
+    temp = previos;
+    previos = current;
+    current = temp + current;
+  }
 }
 
-fs.forEach(function(f) {
-  f();
-})
+function letFunc() {
+  let previos = 0;
+  let current = 1;
+  for (let i = 0; i < n; i += 1) {
+    let temp = previos;
+    previos = current;
+    current = temp + current;
+  }
+}
