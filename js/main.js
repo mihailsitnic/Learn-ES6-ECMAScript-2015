d74611d6d83a53c3c19e70bf7b37756bcc0a2b65
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-02 18:01:58 +0200

    06-egghead-ecmascript-6-object-enhancements-in-es6

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 'use strict';
 
-let recive = (complete = () => console.log('complete')) => complete();
-recive();
+var color = "red";
+var speed = 10;
+
+var car = {
+  color,
+  speed,
+  go() {
+    console.log('vroom');
+  }
+}
+
+console.log(car.color);
+console.log(car.speed);
+car.go();
