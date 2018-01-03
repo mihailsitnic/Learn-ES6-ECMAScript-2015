577c155ffda86c2375875b88cf5ce0634d6f3b96
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-03 15:28:50 +0200

    13-egghead-ecmascript-6-generators__first

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 'use strict';
-// promises with ES6(2015)
-var d = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (true) {
-      resolve('Hello, World!');
-    } else {
-      reject('No, bueno');
-    }
-  }, 2000);
-});
 
-d.then((data) => console.log('success : ', data)).
-  catch((error) => console.error('error : ', error));
-// d.catch((error) => console.error('error : ', error));
+function* greet() {
+  console.log(`You called 'next()'`);
+  yield 'Hello';
+}
+
+let greeter = greet();
+console.log(greeter);
+
+let next = greeter.next();
+console.log(next);
+
+let done = greeter.next();
+console.log(done);
